846f8196cee4c68ba59edae518dfde127f0eb70d
Uzma imran <148060110+uzmaimran970@users.noreply.github.com>
2024-03-08 16:41:50 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+23.; // Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// creating variable 
+let car = 'subaru';
+// 1st true condition
+console.log("is car == 'subaru'? i pridict true");
+console.log(car == 'subaru');
+// 2nd true condition
+console.log("is car === 'subaru'? i pridict true");
+console.log(car === 'subaru');
+//3rd true condition
+console.log("is car != 'suzuki'? i pridict true");
+console.log(car != 'suzuki');
+//4th true condition
+console.log("is car !== 'honda'? i pridict true");
+console.log(car !== 'honda');
+// 5th true condition
+console.log("is car == 'SUBARU'? i pridict true");
+console.log(car.toUpperCase() == 'SUBARU');
+// 6th false condition
+console.log("is car == 'SUBARU'? i pridict true");
+console.log(car == 'SUBARU');
+//7th false condition
+console.log("is car === 'SUBARU'? i pridict true");
+console.log(car === 'SUBARU');
+//8th false condition
+console.log("is car === 'train'? i pridict true");
+console.log(car === 'train');
+//9th false condition
+console.log("is car === 'bus'? i pridict true");
+console.log(car === 'bus');
+//10th false condition
+console.log("is car === 'cycle'? i pridict true");
+console.log(car === 'cycle');
